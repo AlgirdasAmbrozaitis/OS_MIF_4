c872ed7ca3c56bcc5672a3e7263f601369e2ee77
eimantas <eimantas@Z50>
2016-05-21 17:50:47 +0300

    update

--- src/os/ProcessDescriptor.java
+++ src/os/ProcessDescriptor.java
@@ -18,12 +18,12 @@ public class ProcessDescriptor {
     private int priority; // Proceso prioritetas
     private int father_processor; // tėvinio proceso ID
 
-    private ArrayList<Integer> sons_processes; // sūnų sąrašas
-    private ArrList resource; // esami proceso resursai
+    private ArrayList<Integer> sons_processes = new ArrayList(); // sūnų sąrašas
+    private ArrList resource = new ArrList(); // esami proceso resursai
     private int list_where_process_is; // nuoroda, kuriame sąraše esti procesas (LPS,PPS)
     private int created_resourses; // sukurtų resursų sąrašas
     
-    private ArrayList<Integer> operating_memory; // operatyvioji atmintis
+    private ArrayList<Integer> operating_memory  = new ArrayList(); // operatyvioji atmintis
     
     CPU cpu = new CPU();
     
@@ -31,7 +31,10 @@ public class ProcessDescriptor {
     {
         
     }
-    // Nustatomos registrų reikšmės
+    
+    /*
+    Proceso veikimui užstatomos/gaunamos realios mašinos registrų reikšmės
+    */
     public void setCPU(){
         
         cpu.setRegisterR(OS.realMachine.getRegisterR());
@@ -45,9 +48,92 @@ public class ProcessDescriptor {
         cpu.setRegisterC(OS.realMachine.isRegisterC());
         cpu.setRegisterMOD(OS.realMachine.isRegisterMOD());   
     }
+    
     public void getCpu()
     {
         OS.realMachine.setRegisterR(cpu.getRegisterR());
+        OS.realMachine.setRegisterPTR(cpu.getRegisterPTR());
+        OS.realMachine.setRegisterIC(cpu.getRegisterIC());
+        OS.realMachine.setRegisterSP(cpu.getRegisterSP());
+        OS.realMachine.setRegisterINT(cpu.getRegisterINT());
+        OS.realMachine.setRegisterCT(cpu.getRegisterCT());
+        OS.realMachine.setRegisterPI(cpu.getRegisterPI());
+        OS.realMachine.setRegisterSI(cpu.getRegisterSI());
+        OS.realMachine.setRegisterC(cpu.isRegisterC());
+        OS.realMachine.setRegisterMOD(cpu.isRegisterMOD());
     }
+
+    public static int getProcess_id_counter() {
+        return process_id_counter;
+    }
+
+    public static void setProcess_id_counter(int process_id_counter) {
+        ProcessDescriptor.process_id_counter = process_id_counter;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public int getFather_processor() {
+        return father_processor;
+    }
+
+    public void setFather_processor(int father_processor) {
+        this.father_processor = father_processor;
+    }
+
+    public ArrayList<Integer> getSons_processes() {
+        return sons_processes;
+    }
+
+    public void setSons_processes(ArrayList<Integer> sons_processes) {
+        this.sons_processes = sons_processes;
+    }
+
+    public ArrList getResource() {
+        return resource;
+    }
+
+    public void setResource(ArrList resource) {
+        this.resource = resource;
+    }
+
+    public int getList_where_process_is() {
+        return list_where_process_is;
+    }
+
+    public void setList_where_process_is(int list_where_process_is) {
+        this.list_where_process_is = list_where_process_is;
+    }
+
+    public int getCreated_resourses() {
+        return created_resourses;
+    }
+
+    public void setCreated_resourses(int created_resourses) {
+        this.created_resourses = created_resourses;
+    }
+
+    public ArrayList<Integer> getOperating_memory() {
+        return operating_memory;
+    }
+
+    public void setOperating_memory(ArrayList<Integer> operating_memory) {
+        this.operating_memory = operating_memory;
+    }
+    
     
 }
